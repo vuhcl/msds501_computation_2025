f7fe9665cd54e3a258ea43998629284f7d8929e4
Diane Woodbridge <dwoodbridge@usfca.edu>
2025-07-08 14:54:55 -0700

    ex03.sh updated

--- Day1/ex03.sh
+++ Day1/ex03.sh
@@ -1,14 +1,14 @@
 
-# nums=(10 11 12 13)
-# threshold=12
+nums=(10 11 12 13)
+threshold=12
 
-# for num in ${nums[@]}
-#     do
-#     if [ $num -gt $threshold ]
-#     then
-#         echo $num
-#     fi
-# done
+for num in ${nums[@]}
+    do
+    if [ $num -gt $threshold ]
+    then
+        echo $num
+    fi
+done
 
 # OR
 
